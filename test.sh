94be3d3b815e31d9321aafe4a941275a1dbe3eae
teradiot <teradiot@gmail.com>
2017-09-07 12:46:38 +0900

    moved Dockerfiles for each version

--- test.sh
+++ test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
+VERSION=$1
 
-docker build -t alpine-ruby-libv8 .
+docker build -t alpine-ruby-libv8 -f $VERSION/Dockerfile .
 
 docker run --rm -it alpine-ruby-libv8 sh -c 'apk add build-base && gem install mini_racer && ruby -rmini_racer -e "p MiniRacer::Context.new.eval(%Q!7 * 6!)"'
